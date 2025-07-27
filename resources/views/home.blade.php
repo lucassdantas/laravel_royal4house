36d9d6a44c3a717431d374b76a723fb174a05d63
Lucas Dantas <lucasdantasprogramador@gmail.com>
2025-07-27 13:43:26 -0300

    add tail wind and buildin header

---
+++ resources/views/home.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.default-layout', ['pageTitle' => 'Royal 4 House'])
+@section('content')
+<div>
+  <h1>home</h1>
+</div>
+
+@endsection
---
+++ resources/views/layouts/header.blade.php
@@ -0,0 +1,20 @@
+<header class='flex'>
+  <div>
+    <img src='' alt='Logo royal4house'/>
+  </div>
+  <div>
+    <nav>
+      <ul class='flex'>
+        <li><a href='/'>Início</a></li>
+        <li><a href='/condominios'>Condomínios</a></li>
+        <li><a href='/bairros'>Bairros</a></li>
+        <li><a href='/sobre'>Sobre nós</a></li>
+        <li><a href='/contato'>Contato</a></li>
+        <li><a href='/anuncie'>Anuncie</a></li>
+        <li><a href='/blog'>Blog</a></li>
+      </ul>
+    </nav>
+  </div>
+  <div></div>
+
+</header>
